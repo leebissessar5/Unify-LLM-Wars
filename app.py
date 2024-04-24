dc5ae0a5a20bcd0ede539209cd61939e7c1c13c6
leebissessar5 <leebissessar@gmail.com>
2024-04-24 15:07:21 -0300

    initial attempt using ChatBot

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
 import streamlit as st
+from unify import ChatBot
 
 # Function to input API keys and endpoints
 def input_fields():
     with st.sidebar:
         st.header("Configuration")
         unify_api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key")
-        endpoint1 = st.text_input("Endpoint 1*", placeholder="model@provider", value="gpt-3.5-turbo@openai")
+        endpoint1 = st.text_input("Endpoint 1*", placeholder="model@provider", value="claude-3-haiku@anthropic")
         endpoint2 = st.text_input("Endpoint 2*", placeholder="model@provider", value="mixtral-8x7b-instruct-v0.1@together-ai")
         show_credits = st.toggle("Show Credit Usage", value=False)
         return unify_api_key, endpoint1, endpoint2, show_credits
 
 # Function to display chat interface
-def chat_interface():    
+def chat_interface(prompt, chatbot1, chatbot2):    
     # Display chat messages from history
-    for message in st.session_state.messages:
-        if message['role'] == 'user':
+    for msg1, msg2 in zip(chatbot1._message_history, chatbot2._message_history):
+        # both chatbots recieve the same user input
+        if msg1['role'] == 'user':
             with st.chat_message("User", avatar='🧑‍💻'):
-                st.markdown(message['content'])
-        elif message['role'] == 'ai':
+                st.markdown(msg1['content'])
+        elif msg1['role'] == 'assistant':
             dual_chat = st.columns(2)
             with dual_chat[0].chat_message("AI 1", avatar='🤖'):
-                st.markdown(message['content'][0])
+                st.markdown(msg1['content'])
             with dual_chat[1].chat_message("AI 2", avatar='🤖'):
-                st.markdown(message['content'][1])
+                st.markdown(msg2['content'])
+
+    # stream the current prompt
+    with st.chat_message("User", avatar='🧑‍💻'):
+        st.markdown(prompt)
+    dual_chat = st.columns(2)
+    with dual_chat[0].chat_message("AI 1", avatar='🤖'):
+        st.write_stream(chatbot1._process_input(prompt, show_credits=False, show_provider=False))
+    with dual_chat[1].chat_message("AI 2", avatar='🤖'):
+        st.write_stream(chatbot2._process_input(prompt, show_credits=False, show_provider=False))
 
 def main():
-    st.set_page_config(page_title="LLM Wars", page_icon=":tada:", layout="wide")
+    st.set_page_config(page_title="LLM Wars")
     st.title("LLM Wars")
 
     placeholder = st.empty()
@@ -36,21 +47,21 @@ def main():
         3. Chat Away!
     ''')
 
-    unify_api_key, endpoint1, endpoint2, show_credits = input_fields()
+    chatbot1, chatbot2 = None, None
 
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
+    unify_api_key, endpoint1, endpoint2, show_credits = input_fields()
+    ready = unify_api_key and endpoint1 and endpoint2
+
+    if ready:
+        chatbot1 = ChatBot(unify_api_key, endpoint1)
+        chatbot2 = ChatBot(unify_api_key, endpoint2)
+        if show_credits:
+            st.sidebar.write(f"Credit Balance: ${chatbot1._get_credits():.2g}")
 
     if user_input := st.chat_input("Type your message here..."):
         if unify_api_key and endpoint1 and endpoint2:
             placeholder.empty()
-            st.session_state.messages.append({"role": "user", "content": user_input})
-            
-            ai_response1 = "AI 1 says Hello!"
-            ai_response2 = "AI 2 also says Hello!"
-            st.session_state.messages.append({"role": "ai", "content": [ai_response1, ai_response2]})
-            
-            chat_interface()
+            chat_interface(user_input, chatbot1, chatbot2)
         else:
             st.sidebar.warning("Please enter the Unify API Key and Endpoints to proceed.")
 
