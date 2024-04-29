95ca0bd30804e98572854d2951cd88499ac58280
leebissessar5 <leebissessar@gmail.com>
2024-04-29 17:12:34 -0300

    add expander

--- config.py
+++ config.py
@@ -2,53 +2,100 @@ import streamlit as st
 import json
 import requests
 import time
+from typing import List, Dict, Tuple
 
 base_url = "https://api.unify.ai/v0"
 
-@st.cache_data
-def load_models():
-    with open('models.json', 'r') as f:
-        data = json.load(f)
-    return data['models']
+if 'selections' not in st.session_state:
+    st.session_state['selections'] = {}
 
-def get_providers(model: str):
+
+def update_selection(key: str, model_or_provider: str, value: str) -> None:
+    '''
+    Update the selection dictionary
+
+    Args:
+        key (str): The key to update
+        model_or_provider (str): The model or provider to update
+        value (str): The value to update
+    '''
+    if key not in st.session_state["selections"]:
+        st.session_state["selections"][key] = {}
+    st.session_state["selections"][key][model_or_provider] = value
+
+
+@st.cache_data
+def load_models() -> List[Dict[str, str]]:
+    '''
+    Load models from models.json
+
+    Returns:
+        List[Dict[str, str]]: List of models
+    '''
+    with open("models.json", "r") as f:
+        data = json.load(f)
+    return data["models"]
+
+
+def get_providers(model: str) -> List[str]:
+    '''
+    Get providers for a given model
+
+    Args:
+        model (str): The model name
+
+    Returns:
+        List[str]: List of providers
+    '''
     url = f"{base_url}/endpoints_of"
     response = requests.get(url, params={"model": model})
     if response.status_code == 200:
-        providers = [provider.split('@')[1] for provider in json.loads(response.text)]
+        providers = [provider.split("@")[1]
+                     for provider in json.loads(response.text)]
         return providers
     else:
         st.error("Failed to fetch providers from API.")
         return []
 
-def update_models():
+
+def update_models() -> None:
+    '''
+    Update models.json with providers
+    '''
     model_names = list_models()
     models = []
-    
+
     if not model_names:
         st.error("No models found to update.")
         return
-    
+
     progress_bar = st.progress(0)
     message_placeholder = st.empty()  # Placeholder for the message
     total_models = len(model_names)
-    
+
     for index, model in enumerate(model_names):
         message_placeholder.text(f"Searching providers for {model}...")
-        
+
         providers = get_providers(model)
-        models.append({'name': model, 'providers': providers})
-        
+        models.append({"name": model, "providers": providers})
+
         # Update the progress bar
         progress_bar.progress((index + 1) / total_models)
-    
-    with open('models.json', 'w') as f:
+
+    with open("models.json", "w") as f:
         json.dump({"models": models}, f, indent=4)
-    
+
     st.success("Models and providers updated successfully!")
     message_placeholder.empty()  # Clear the message placeholder
 
-def list_models():
+
+def list_models() -> List[str]:
+    '''
+    List models from Unify API
+
+    Returns:
+        List[str]: List of models
+    '''
     url = f"{base_url}/models"
     response = requests.get(url)
     if response.status_code == 200:
@@ -57,27 +104,50 @@ def list_models():
         st.error("Failed to fetch models from API.")
         return []
 
-def input_fields():
+
+def input_fields() -> Tuple[str, Dict[str, str]]:
+    """
+    Input fields for the app
+
+    Returns:
+        Tuple[str, Dict[str, str]]: API key, endpoints
+    """
     with st.sidebar:
         st.header("Configuration")
-        api_key = st.text_input("Unify API Key*", type="password", placeholder="Enter Unify API Key")
-
-        if st.button("Update Models from API"):
-            update_models()
-            time.sleep(1)  # Add a delay to indicate the update is complete
-            st.rerun()
+        api_key = st.text_input(
+            "Unify API Key*",
+            type="password",
+            placeholder="Enter Unify API Key"
+        )
 
         models = load_models()
 
         endpoints = {}
         for key in ["LLM1", "LLM2", "Judge"]:
-            model_names = [model['name'] for model in models]
-            selected_model = st.selectbox(f"{key} Model", options=model_names, key=f"{key}_model")
-            if selected_model:
-                model_details = next((item for item in models if item["name"] == selected_model), None)
-                if model_details:
-                    providers = model_details['providers']
-                    selected_provider = st.selectbox(f"{key} Provider", options=providers, key=f"{key}_provider")
-                    endpoints[key] = f"{selected_model}@{selected_provider}"
+            expander = st.expander(f"{key} Configuration", expanded=False)
+            with expander:
+                model_names = [model["name"] for model in models]
+                selected_model = st.selectbox(
+                    "Model", options=model_names, key=f"{key}_model"
+                )
+                if selected_model:
+                    model_details = next(
+                        (item
+                         for item in models if item["name"] == selected_model),
+                        None,
+                    )
+                    if model_details:
+                        providers = model_details["providers"]
+                        selected_provider = st.selectbox(
+                            "Provider",
+                            options=providers,
+                            key=f"{key}_provider"
+                        )
+                        endpoints[key] = f"{selected_model}@{selected_provider}"
+
+        if st.button("Update Models from API"):
+            update_models()
+            time.sleep(1)  # Add a delay to indicate the update is complete
+            st.rerun()
 
         return api_key, endpoints
--- main.py
+++ main.py
@@ -3,28 +3,48 @@ from unify import ChatBot
 from battle import llm_battle
 from config import input_fields
 
-for key in ['LLM1', 'LLM2', 'Judge', 'New Chat', 'Done', 'Next Round', 'Valid Key', 'previous_api_key']:
+for key in [
+    "LLM1",
+    "LLM2",
+    "Judge",
+    "New Chat",
+    "Done",
+    "Next Round",
+    "Valid Key",
+    "previous_api_key",
+]:
     if key not in st.session_state:
         st.session_state[key] = None
-        if key == 'New Chat':
+        if key == "New Chat":
             st.session_state[key] = True
-        if key == 'Valid Key':
+        if key == "Valid Key":
             st.session_state[key] = False
 
+
 def new_chat_cb():
-    st.session_state['New Chat'] = True
-    st.session_state['Done'] = True
+    st.session_state["New Chat"] = True
+    st.session_state["Done"] = True
+
 
 def next_round_cb(yes):
-    st.session_state['Next Round'] = yes
+    st.session_state["Next Round"] = yes
+
 
 def chatbots_exists():
-    return st.session_state['LLM1'] and st.session_state['LLM2'] and st.session_state['Judge']
+    return (
+        st.session_state["LLM1"]
+        and st.session_state["LLM2"]
+        and st.session_state["Judge"]
+    )
+
 
 def chatbots_empty():
-    return chatbots_exists() and (not st.session_state['LLM1']._message_history \
-             or not st.session_state['LLM2']._message_history \
-             or not st.session_state['Judge']._message_history)
+    return chatbots_exists() and (
+        not st.session_state["LLM1"]._message_history
+        or not st.session_state["LLM2"]._message_history
+        or not st.session_state["Judge"]._message_history
+    )
+
 
 def main():
     st.set_page_config(page_title="LLM Wars")
@@ -33,77 +53,96 @@ def main():
     api_key, endpoints = input_fields()
 
     # Check if the API key has changed
-    if api_key != st.session_state['previous_api_key']:
-        st.session_state['previous_api_key'] = api_key
-        st.session_state['Valid Key'] = False  # Reset valid key state
+    if api_key != st.session_state["previous_api_key"]:
+        st.session_state["previous_api_key"] = api_key
+        st.session_state["Valid Key"] = False  # Reset valid key state
         # erase chatbots
-        for key in ['LLM1', 'LLM2', 'Judge']:
+        for key in ["LLM1", "LLM2", "Judge"]:
             st.session_state[key] = None
         st.rerun()  # Rerun the app to revalidate the new API key
 
-    if 'new_chat_cb' not in st.session_state:
-        st.session_state['new_chat_cb'] = new_chat_cb
+    if "new_chat_cb" not in st.session_state:
+        st.session_state["new_chat_cb"] = new_chat_cb
 
-    if 'next_round_cb' not in st.session_state:
-        st.session_state['next_round_cb'] = next_round_cb
+    if "next_round_cb" not in st.session_state:
+        st.session_state["next_round_cb"] = next_round_cb
 
-    if 'prev_content' not in st.session_state:
-        st.session_state['prev_content'] = []
+    if "prev_content" not in st.session_state:
+        st.session_state["prev_content"] = []
 
-    if 'round_number' not in st.session_state:
-        st.session_state['round_number'] = 1
+    if "round_number" not in st.session_state:
+        st.session_state["round_number"] = 1
 
-    if 'prompt' not in st.session_state:
-        st.session_state['prompt'] = ''
+    if "prompt" not in st.session_state:
+        st.session_state["prompt"] = ""
 
     # verify that all required fields are filled in
     if api_key and endpoints and all(endpoints.values()):
         # create ChatBot instances if they don't exist yet
         for keys in endpoints:
-            if not st.session_state[keys] or st.session_state[keys].endpoint != endpoints[keys]:
+            if (
+                not st.session_state[keys]
+                or st.session_state[keys].endpoint != endpoints[keys]
+            ):
                 st.session_state[keys] = ChatBot(api_key, endpoints[keys])
                 # Reset the chat if any of the ChatBots have been created
-                st.session_state['New Chat'] = True
-                
-        try:
-            st.session_state['credits'] = st.session_state['LLM1']._get_credits()
-            st.session_state['Valid Key'] = True
-        except:
-            st.session_state['Valid Key'] = False
+                st.session_state["New Chat"] = True
 
+        try:
+            st.session_state["credits"] = st.session_state["LLM1"]._get_credits()
+            st.session_state["Valid Key"] = True
+        except Exception:
+            st.session_state["Valid Key"] = False
 
     # create empty placeholders for the instructions and start button
     placeholder, btn_placeholder = st.empty(), st.empty()
     # Show instructions if no ChatBots exist yet or the chat history is fresh
-    if st.session_state['New Chat']:
-        placeholder.write('''
-            Usage: 
+    if st.session_state["New Chat"]:
+        placeholder.write(
+            """
+            Usage:
             1. Input your **Unify API Key** (if not stored as secrets, use the sidebar). If you don’t have one yet, log in to the [console](https://console.unify.ai/) to get yours.
             2. Choose your Endpoints (i.e. **Model and Provider**, in the [benchmark interface](https://unify.ai/hub)).
             3. Click **Start Battle**.
-        ''')
+        """
+        )
 
         # show the start button only if the chat is fresh
         if btn_placeholder.button("Start Battle"):
-            if st.session_state['Valid Key']:
+            if st.session_state["Valid Key"]:
                 if chatbots_exists():
                     placeholder.empty()
                     btn_placeholder.empty()
-                    st.session_state['New Chat'] = False
-                    llm_battle(st.session_state['LLM1'], st.session_state['LLM2'], st.session_state['Judge'], new_chat=True)
+                    st.session_state["New Chat"] = False
+                    llm_battle(
+                        st.session_state["LLM1"],
+                        st.session_state["LLM2"],
+                        st.session_state["Judge"],
+                        new_chat=True,
+                    )
                 else:
-                    st.warning("Please enter the Unify API Key on the sidebar to proceed.")
+                    st.warning(
+                        "Please enter the Unify API Key on the sidebar to proceed."
+                    )
             else:
                 st.error("Invalid key. Please check your Unify API Key.")
     else:
-        llm_battle(st.session_state['LLM1'], st.session_state['LLM2'], st.session_state['Judge'], new_chat=False, next_round=st.session_state['Next Round'])
+        llm_battle(
+            st.session_state["LLM1"],
+            st.session_state["LLM2"],
+            st.session_state["Judge"],
+            new_chat=False,
+            next_round=st.session_state["Next Round"],
+        )
 
     if chatbots_exists() and st.sidebar.toggle("Show Credit Balance"):
-        if 'credits' in st.session_state:
-            st.sidebar.write(f"Credit Balance: ${st.session_state['credits']:.2f}")
+        if "credits" in st.session_state:
+            st.sidebar.write(
+                f"Credit Balance: ${st.session_state['credits']:.2f}"
+            )
 
-    if st.session_state['Done']:
-        st.session_state['Done'] = False
+    if st.session_state["Done"]:
+        st.session_state["Done"] = False
         st.rerun()
 
 
